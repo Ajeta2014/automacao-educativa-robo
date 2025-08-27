4c8751c904ced070f24b6600ed13cc2a5a6b3e54
Ajeta2014 <ajetaangola@gmail.com>
2025-08-27 16:27:12 +0100

    Update ambiente.py

--- ambiente.py
+++ ambiente.py
@@ -10,31 +10,26 @@ class Ambiente:
 
     def desenha(self, robo):
         plt.figure(figsize=(6,6))
-        grid = [[0]*self.grid_size for _ in range(self.grid_size)]
-
         # Obstáculos
         for ox, oy in self.obstaculos:
             plt.scatter(ox, oy, c="black", s=200, label="Obstáculo")
-
         # Metas
         for mx, my in self.metas:
             plt.scatter(mx, my, c="green", s=200, label="Meta")
-
         # Linha de percurso
         if self.linha:
             lx, ly = zip(*self.linha)
             plt.plot(lx, ly, 'b--', lw=2, label="Linha")
-
         # Trajetória do robô
         if len(robo.trajetoria) > 1:
             tx, ty = zip(*robo.trajetoria)
             plt.plot(tx, ty, 'orange', lw=2, label="Trajetória")
-
         # Robô ilustrado
         plt.scatter(robo.x, robo.y, c="red", s=300, edgecolors="black", marker="o", label="Robô")
-        plt.title("Mini Laboratório de Robótica – Módulo Interativo")
+        plt.title("Mini Laboratório de Robótica")
         plt.xlim(-0.5, self.grid_size-0.5)
         plt.ylim(-0.5, self.grid_size-0.5)
         plt.grid(True)
         plt.legend()
         st.pyplot(plt)
+
