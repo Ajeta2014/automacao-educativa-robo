369041471c0de8fcbd4b443e3fa8ad2fe66d416c
Ajeta2014 <ajetaangola@gmail.com>
2025-08-27 16:21:01 +0100

    Create modulo.py

---
+++ modulo.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+class Modulo:
+    def __init__(self, nome, missao, objetivos):
+        self.nome = nome
+        self.missao = missao
+        self.objetivos = objetivos
+
+    def mostrar_info(self):
+        st.sidebar.subheader(f"Módulo {self.nome}")
+        st.sidebar.markdown(f"**Missão:** {self.missao}")
+        st.sidebar.markdown("**Objetivos:**")
+        for obj in self.objetivos:
+            st.sidebar.markdown(f"- {obj}")
+v
