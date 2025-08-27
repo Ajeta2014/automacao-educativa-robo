c272b2bed4622eb989480f7e15365ade810f6605
Ajeta2014 <ajetaangola@gmail.com>
2025-08-27 16:17:14 +0100

    Update ambiente.py

--- ambiente.py
+++ ambiente.py
@@ -2,33 +2,39 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 class Ambiente:
-    def __init__(self, grid_size=10, obstaculos=None, linha=None):
+    def __init__(self, grid_size=10, obstaculos=None, linha=None, metas=None):
         self.grid_size = grid_size
         self.obstaculos = obstaculos or []
         self.linha = linha or []
+        self.metas = metas or []
 
     def desenha(self, robo):
-        grid = [[0]*self.grid_size for _ in range(self.grid_size)]
-        for ox, oy in self.obstaculos:
-            grid[oy][ox] = 1
-
         plt.figure(figsize=(6,6))
-        plt.imshow(grid, cmap="binary", origin="lower")
+        grid = [[0]*self.grid_size for _ in range(self.grid_size)]
 
-        # Linha
+        # Obstáculos
+        for ox, oy in self.obstaculos:
+            plt.scatter(ox, oy, c="black", s=200, label="Obstáculo")
+
+        # Metas
+        for mx, my in self.metas:
+            plt.scatter(mx, my, c="green", s=200, label="Meta")
+
+        # Linha de percurso
         if self.linha:
             lx, ly = zip(*self.linha)
             plt.plot(lx, ly, 'b--', lw=2, label="Linha")
 
-        # Trajetória
+        # Trajetória do robô
         if len(robo.trajetoria) > 1:
             tx, ty = zip(*robo.trajetoria)
             plt.plot(tx, ty, 'orange', lw=2, label="Trajetória")
 
-        # Robô
-        plt.scatter(robo.x, robo.y, c="red", s=200, edgecolors="black", marker="o", label="Robô")
-        plt.title("Mini Laboratório de Robótica")
+        # Robô ilustrado
+        plt.scatter(robo.x, robo.y, c="red", s=300, edgecolors="black", marker="o", label="Robô")
+        plt.title("Mini Laboratório de Robótica – Módulo Interativo")
+        plt.xlim(-0.5, self.grid_size-0.5)
+        plt.ylim(-0.5, self.grid_size-0.5)
+        plt.grid(True)
         plt.legend()
         st.pyplot(plt)
-
-
