113ca7e040e3267855845d529328a45606d1fcb5
Ajeta2014 <ajetaangola@gmail.com>
2025-08-27 16:17:03 +0100

    Update robo.py

--- robo.py
+++ robo.py
@@ -1,13 +1,19 @@
+from PIL import Image, ImageDraw
+import streamlit as st
+
 class Robo:
-    def __init__(self):
+    def __init__(self, grid_size=10):
         self.x = 0
         self.y = 0
+        self.grid_size = grid_size
         self.trajetoria = [(self.x, self.y)]
         self.pontos = 0
         self.colisoes = 0
+        self.direcao = "N"  # N, S, E, W, NE, NW, SE, SW
 
-    def move(self, direcao, obstaculos=None, grid_size=10):
+    def move(self, direcao, obstaculos=None):
         dx, dy = 0, 0
+        self.direcao = direcao
         if direcao == "N": dy = 1
         elif direcao == "S": dy = -1
         elif direcao == "E": dx = 1
@@ -20,7 +26,7 @@ class Robo:
         nx, ny = self.x + dx, self.y + dy
         if obstaculos and (nx, ny) in obstaculos:
             self.colisoes += 1
-        elif 0 <= nx < grid_size and 0 <= ny < grid_size:
+        elif 0 <= nx < self.grid_size and 0 <= ny < self.grid_size:
             self.x, self.y = nx, ny
             self.trajetoria.append((self.x, self.y))
             self.pontos += 1
@@ -30,4 +36,4 @@ class Robo:
         self.trajetoria = [(self.x, self.y)]
         self.pontos = 0
         self.colisoes = 0
-
+        self.direcao = "N"
